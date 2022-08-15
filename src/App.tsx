7ebbf3ab6825a0f86c334775c4a13046162e9918
Neoxs <m.abdelkaderkharoubi@esi-sba.dz>
2022-08-15 15:57:08 +0100

    ADD the main structure for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <div className="header">
+        <h1 className="logo">Mawakit</h1>
+        <p className="subtitle">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ex
+          iure esse obcaecati at omnis quam asperiores enim eaque excepturi!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="search">
+        <input type="date" className="search__date" />
+        <input type="text" className="search__country" />
+        <button type="submit">show</button>
+      </div>
+      <div className="result">
+        <span className="result__sunrise">Sunrise is at: 7:54</span>
+        <span className="result__sunset">Sunrise is at: 16:06</span>
+      </div>
     </div>
   );
 }
